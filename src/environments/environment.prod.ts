71e387d959ae2400cf00b2b2d30cee9aa7b7bd6e
idan <idanco77@gmail.com>
2020-06-23 01:10:54 +0300

    m

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiUrl: 'http://www.idan.work/book-a-train-calculator-api/api',
+  apiUrl: 'http://www.idan.work/book-a-train-calculator-api/public/api',
   production: true
 };
